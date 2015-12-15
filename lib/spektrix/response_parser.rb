228586ea7c468ccafbf1f914c041ded130343db6
Ed Jones <ed@errorstudio.co.uk>
2015-12-15 22:32:25 +0000

    tidy up response parser

--- lib/spektrix/response_parser.rb
+++ lib/spektrix/response_parser.rb
@@ -1,21 +1,9 @@
 module Spektrix
   class ResponseParser < ::Faraday::Response::Middleware
     def on_complete(env)
-      # body = env[:body].dup
-      # body.extend ::DeepSymbolizable
-      # # body = body.deep_symbolize {|k| k.downcase.underscore }
-      # puts "\n\n" + body.inspect
-      # env[:body] = {
-      #   data: body[:events],
-      #   errors: {},
-      #   metadata: {}
-      # }
 
       doc = Nokogiri::XML(env[:body])
       doc.remove_namespaces!
-      # data = doc.xpath("//Event").collect do |e|
-      #   Hash.from_xml(env[:body]).values.first
-      # end
 
       data = Hash.from_xml(doc.to_s).deep_symbolize {|k| k.underscore.to_sym}
       data = data.values.first.values.flatten
@@ -28,8 +16,6 @@ module Spektrix
       env[:body] = {
         data: data
       }
-      puts "\n\n#{env}"
-
     end
   end
 end
