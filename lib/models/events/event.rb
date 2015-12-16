0529e8eb1d0713e4df46de350b53fb18054466a6
Ed Jones <ed@errorstudio.co.uk>
2015-12-16 22:40:12 +0000

    Relate instances to Event

--- lib/models/events/event.rb
+++ lib/models/events/event.rb
@@ -1,8 +1,13 @@
 module Spektrix
   module Events
+    # An event. This hits the events endpoint on Spektrix and returns a collection.
     class Event
       include Spektrix::Base
       collection_path "events"
+
+      def instances
+        Instance.where(event_id: self.id).to_a
+      end
     end
   end
 end
---
+++ lib/models/events/instance.rb
@@ -0,0 +1,10 @@
+module Spektrix
+  module Events
+    # An event instance.
+    class Instance
+      include Spektrix::Base
+      collection_path "instances"
+
+    end
+  end
+end
