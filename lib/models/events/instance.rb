30a094352860161074881a0a9e2222c5e92c4035
Ed Jones <ed@errorstudio.co.uk>
2015-12-18 12:36:14 +0000

    Cast string dates to DateTime

--- lib/models/events/instance.rb
+++ lib/models/events/instance.rb
@@ -5,6 +5,25 @@ module Spektrix
       include Spektrix::Base
       collection_path "instances"
 
+      after_find ->(r) {
+        [:start,
+         :start_utc,
+         :start_selling_at,
+         :start_selling_at_utc,
+         :stop_selling_at,
+         :stop_selling_at_utc
+        ].each do |field|
+          if r.respond_to?(field)
+            r.send(:"#{field}=",DateTime.parse(r.send(
+              field)))
+          end
+
+        end
+      }
+
+      def status
+        InstanceStatus.where(instance_id: self.id).first
+      end
     end
   end
 end
