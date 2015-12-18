2d7efffe73c359906517fbbdfdc20eae87a9b74a
Ed Jones <ed@errorstudio.co.uk>
2015-12-18 12:35:20 +0000

    Move plan to seating namespace

---
+++ lib/models/seating/plan.rb
@@ -0,0 +1,14 @@
+module Spektrix
+  module Tickets
+    class Plan
+      include Spektrix::Base
+      collection_path "plans"
+
+      after_find :handle_recursive_plans
+
+      # Plans can have sub-plans, but the sub-plans are in the top-level array. We need to remove them from the top-level array and inject into the subplan node for the given parent plan. Note that sub-plans are recursive.
+
+
+    end
+  end
+end
--- lib/models/tickets/plan.rb
+++
@@ -1,8 +0,0 @@
-module Spektrix
-  module Tickets
-    class Plan
-      include Spektrix::Base
-      collection_path "plans"
-    end
-  end
-end
