ce5d2dd50e1b96106f1848fd99f18ba7100f07e7
Ed Jones <ed@errorstudio.co.uk>
2015-12-18 12:36:28 +0000

    Tickets => Seating

--- lib/models/seating/plan.rb
+++ lib/models/seating/plan.rb
@@ -1,5 +1,5 @@
 module Spektrix
-  module Tickets
+  module Seating
     class Plan
       include Spektrix::Base
       collection_path "plans"
@@ -7,6 +7,7 @@ module Spektrix
       after_find :handle_recursive_plans
 
       # Plans can have sub-plans, but the sub-plans are in the top-level array. We need to remove them from the top-level array and inject into the subplan node for the given parent plan. Note that sub-plans are recursive.
+
 
 
     end
