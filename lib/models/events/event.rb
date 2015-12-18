04b3be8f69dff6e2a97759334d0c70871410e228
Ed Jones <ed@errorstudio.co.uk>
2015-12-18 12:35:41 +0000

    Set event duration to ActiveSupport::Duration

--- lib/models/events/event.rb
+++ lib/models/events/event.rb
@@ -5,9 +5,13 @@ module Spektrix
       include Spektrix::Base
       collection_path "events"
 
+      after_find -> (r) { r.duration = r.duration.to_i.minutes }
+
       def instances
         Instance.where(event_id: self.id).to_a
       end
+
+
     end
   end
 end
