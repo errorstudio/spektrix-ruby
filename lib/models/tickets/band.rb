3309dbdde2a10f3be1b8c699ea865b1c4bf9ec52
Ed Jones <ed@errorstudio.co.uk>
2016-01-09 22:35:28 +0000

    filter by band id

--- lib/models/tickets/band.rb
+++ lib/models/tickets/band.rb
@@ -7,6 +7,11 @@ module Spektrix
       def to_s
         self.name
       end
+
+      # The API doesn't allow you to filter by band_id, so we get all and find() in ruby
+      def self.find(id)
+        all.to_a.find {|b| b.id.to_i == id}
+      end
     end
   end
 end
