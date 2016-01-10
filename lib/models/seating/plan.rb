a4f577e30268c0ea90480a1f8dd5b71d8a3ecf4e
Ed Jones <ed@errorstudio.co.uk>
2016-01-10 12:29:01 +0000

    Find() method in plan

--- lib/models/seating/plan.rb
+++ lib/models/seating/plan.rb
@@ -4,6 +4,18 @@ module Spektrix
       include Spektrix::Base
       collection_path "plans"
 
+      def to_s
+        name
+      end
+
+      def all
+        @all ||= all(all: true)
+      end
+
+      # The API doesn't allow you to filter by band_id, so we get all and find() in ruby
+      def self.find(id)
+        all.to_a.find {|b| b.id.to_i == id}
+      end
 
     end
   end
