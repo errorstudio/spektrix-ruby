21f6837f3d71651cab2177e6924f3d7549a34b10
Ed Jones <ed@errorstudio.co.uk>
2016-01-17 19:53:55 +0000

    Get bands and types only once when iterating through price lists generating prices

--- lib/models/tickets/price_list.rb
+++ lib/models/tickets/price_list.rb
@@ -5,16 +5,26 @@ module Spektrix
       collection_path "price-lists"
 
       after_find ->(r) do
-        bands = Band.all.to_a
-        ticket_types = Type.all.to_a
+        @@bands ||= Band.all.to_a
+        @@ticket_types ||= Type.all.to_a
         if r.respond_to?(:price)
+          r.price = [r.price] unless r.price.is_a?(Array)
           r.prices = r.price.collect do |price|
-            price[:band] = bands.find {|b| b.id == price[:band][:id]}
-            price[:ticket_type] = ticket_types.find {|t| t.id == price[:ticket_type][:id]}
+            price[:band] = @@bands.find {|b| b.id == price[:band][:id]}
+            price[:ticket_type] = @@ticket_types.find {|t| t.id == price[:ticket_type][:id]}
             Price.new(price)
           end
         end
       end
+
+      def self.find(id)
+        all.to_a.find {|r| r.id.to_i == id }
+      end
+
+
+
+
     end
   end
 end
+
